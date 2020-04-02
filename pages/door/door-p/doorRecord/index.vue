1e558289d3f36123f1bb7550082d2bbb89e98144
zzh746292506 <746292506@qq.com>
2020-04-02 17:39:59 +0800

    ''

--- pages/door/door-p/doorRecord/index.vue
+++ pages/door/door-p/doorRecord/index.vue
@@ -1,158 +1,153 @@
 <template>
-  <view>
-    <view class="test"><button type="default" @click="onShowDatePicker('rangetime')">选择日期时间范围搜索</button></view>
-    <mx-date-picker
-      :show="showPicker"
-      :type="type"
-      :value="value"
-      :show-tips="true"
-      :begin-text="'开始'"
-      :end-text="'结束'"
-      :show-seconds="true"
-      @confirm="onSelected"
-      @cancel="onSelected"
-    />
-     <scroll-view @refresherpulling="haha" scroll-y="true" @scrolltolower="showList(true)" class="scroll-h">
-    <view class="qui-page">
-      <no-data v-if="false" msg="暂无数据"></no-data>
-      <view class="list">
-        <view class="th qui-fx-jsa qui-fx-ac qui-fx-jc">
-          <text class="left">时间</text>
-          <view class="md qui-fx-ac qui-fx-jc">
-            <text>进/出</text>
-            <view class="icon qui-fx-ver"></view>
-          </view>
-          <text class="right">地点</text>
-        </view>
-          <view v-for="list in dataList" :key="list.id" class="tbody qui-bd-b qui-fx-jsb">
-            <text class="left">{{ list.name }}</text>
-            <text class="md">{{ list.sex }}</text>
-            <text class="right">{{ list.Organization }}</text>
-          </view>
-      </view>
-    </view>
-    </scroll-view>
-  </view>
+	<view class="">
+		<view class="test"><button type="default" @click="onShowDatePicker('rangetime')">选择日期时间范围搜索</button></view>
+		<mx-date-picker
+			:show="showPicker"
+			:type="type"
+			:value="value"
+			:show-tips="true"
+			:begin-text="'开始'"
+			:end-text="'结束'"
+			:show-seconds="true"
+			@confirm="onSelected"
+			@cancel="onSelected"
+		/>
+		<scroll-view @refresherpulling="haha" scroll-y="true" class="scroll-h">
+			<view class="qui-page">
+				<no-data v-if="false" msg="暂无数据"></no-data>
+				<view class="list">
+					<view class="th qui-fx-jsa qui-fx-ac qui-fx-jc">
+						<text class="left">时间</text>
+						<view class="md qui-fx-ac qui-fx-jc">
+							<text>进/出</text>
+							<view class="icon qui-fx-ver"></view>
+						</view>
+						<text class="right">地点</text>
+					</view>
+					<view v-for="list in dataList" :key="list.id" class="tbody qui-bd-b qui-fx-jsb">
+						<text class="left">{{ list.name }}</text>
+						<text class="md">{{ list.sex }}</text>
+						<text class="right">{{ list.Organization }}</text>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
 </template>
 <script>
 import MxDatePicker from '@/components/mx-datepicker/mx-datepicker.vue';
 import noData from '@/components/no-data/no-data.vue';
 import eventBus from '@u/eventBus.js';
 export default {
-  components: {
-    MxDatePicker,
-    noData
-  },
-  data() {
-    return {
-      showPicker: false,
-      date: '2019/01/01',
-      time: '15:00:12',
-      datetime: '2019/01/01 15:00:12',
-      range: ['2019/01/01', '2019/01/06'],
-      rangetime: ['2019/01/08 14:00', '2019/01/16 13:59'],
-      type: 'rangetime',
-      value: '',
-      dataList: [
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        },
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        },
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        },
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        },
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        },
-        {
-          name: '张三',
-          sex: '女',
-          Organization: '全品文教'
-        }
-      ]
-    };
-  },
-  methods: {
-    addInfo() {
-      uni.navigateTo({
-        url: './add'
-      });
-    },
-    onShowDatePicker(type) {
-      //显示
-      this.type = type;
-      this.showPicker = true;
-      this.value = this[type];
-    },
-    onSelected(e) {
-      //选择
-      this.showPicker = false;
-      if (e) {
-        this[this.type] = e.value;
-        //选择的值
-        console.log('value => ' + e.value);
-        //原始的Date对象
-        console.log('date => ' + e.date);
-      }
-    }
-  },
-  mounted() {}
+	components: {
+		MxDatePicker,
+		noData
+	},
+	data() {
+		return {
+			showPicker: false,
+			date: '2019/01/01',
+			time: '15:00:12',
+			datetime: '2019/01/01 15:00:12',
+			range: ['2019/01/01', '2019/01/06'],
+			rangetime: ['2019/01/08 14:00', '2019/01/16 13:59'],
+			type: 'rangetime',
+			value: '',
+			dataList: [
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教'
+				}
+			]
+		};
+	},
+	methods: {
+		onShowDatePicker(type) {
+			//显示
+			this.type = type;
+			this.showPicker = true;
+			this.value = this[type];
+		},
+		onSelected(e) {
+			//选择
+			this.showPicker = false;
+			if (e) {
+				this[this.type] = e.value;
+				//选择的值
+				console.log('value => ' + e.value);
+				//原始的Date对象
+				console.log('date => ' + e.date);
+			}
+		}
+	},
+	mounted() {}
 };
 </script>
 <style lang="scss" scoped>
 .test {
-  text-align: center;
-  padding: 10px 0;
+	text-align: center;
+	padding: 10px 0;
 }
 button {
-  margin: 20upx;
-  font-size: 28upx;
+	margin: 20upx;
+	font-size: 28upx;
 }
 .list {
-  padding: 25rpx 20rpx;
-  font-size: 28rpx;
-  .th {
-    background: #7b92f5;
-    padding: 20rpx 0;
-    border-radius: 8rpx;
-    color: #fff;
-  }
-  .tbody {
-    position: relative;
-    padding: 25rpx 10rpx;
-  }
-  .tbody:nth-child(even) {
-    background: #f5f5f5;
-  }
-  .left {
-    width: 25%;
-    text-align: center;
-  }
-  .md {
-    width: calc(50% - 40rpx);
-    text-align: center;
-  }
-  .right {
-    width: 25%;
-    text-align: center;
-  }
+	padding: 25rpx 20rpx;
+	font-size: 28rpx;
+	.th {
+		background: #7b92f5;
+		padding: 20rpx 0;
+		border-radius: 8rpx;
+		color: #fff;
+	}
+	.tbody {
+		position: relative;
+		padding: 25rpx 10rpx;
+	}
+	.tbody:nth-child(even) {
+		background: #f5f5f5;
+	}
+	.left {
+		width: 25%;
+		text-align: center;
+	}
+	.md {
+		width: calc(50% - 40rpx);
+		text-align: center;
+	}
+	.right {
+		width: 25%;
+		text-align: center;
+	}
 }
 .scroll-h {
-  height: 100vh;
+	height: 100vh;
 }
 </style>
--- pages/door/door-t/doorManage/detail.vue
+++ pages/door/door-t/doorManage/detail.vue
@@ -1,280 +1,284 @@
 <template>
-  <view class="qui-page">
-     <view class="detail">
-        <!-- <view class="back_" @click="goBack">>返回</view> -->
-        <!-- <text class="text_">小学部教师组</text> -->
-        <view class="btn" @click="add">
-          <button type="primary" size="mini">添加成员</button>
-          
-          </view>
-     </view>
-     <no-data v-if="false" msg="暂无数据"></no-data>
-     <view  class="list">
-       <view class="th qui-fx-jsa qui-fx-ac qui-fx-jc">
-         <text class="left">姓名</text>
-         <view class="md qui-fx-ac qui-fx-jc">
-           <text>性别</text>
-           <view class="icon qui-fx-ver">
-           </view>
-          </view>
-         <text class="right">组织机构</text>
-         <text class="right">手机号</text>
-         <text class="right">操作</text>
-       </view>
-       <scroll-view @refresherpulling="haha" scroll-y="true" @scrolltolower="showList(true)" class="scroll-h">
-         <view  v-for="list in dataList" :key="list.id" class="tbody qui-bd-b qui-fx-jsb" >
-           <text class="left">{{list.name}}</text>
-           <text class="md">{{list.sex}}</text>
-           <text class="right">{{list.Organization}}</text>
-           <text class="right">{{list.tel}}</text>
-           <text class="right" @click="open()">删除</text>
-         </view>  
-       </scroll-view>
-       <uni-popup ref="popup" type="center">
-             <view class="modal-ti">提示</view>
-             <view class="modal-su">确定删除该成员?</view>
-             <view class="modal-btn">
-               <button type="default" size="mini" @click="close()">取消</button>
-               <button type="primary" size="mini" @click="del()">确定</button>
-           </view>
-       </uni-popup>
-     </view>
-  </view>
+	<view class="qui-page">
+		<view class="detail">
+			<view class="btn" @click="add"><button type="primary" size="mini">添加成员</button></view>
+		</view>
+		<no-data v-if="false" msg="暂无数据"></no-data>
+		<view class="list">
+			<view class="th qui-fx-jsa qui-fx-ac qui-fx-jc">
+				<text class="left">姓名</text>
+				<view class="md qui-fx-ac qui-fx-jc">
+					<text>性别</text>
+					<view class="icon qui-fx-ver"></view>
+				</view>
+				<text class="right">组织机构</text>
+				<text class="right">手机号</text>
+				<text class="right">操作</text>
+			</view>
+			<scroll-view @refresherpulling="haha" scroll-y="true"  class="scroll-h">
+				<view v-for="list in dataList" :key="list.id" class="tbody qui-bd-b qui-fx-jsb">
+					<text class="left">{{ list.name }}</text>
+					<text class="md">{{ list.sex }}</text>
+					<text class="right">{{ list.Organization }}</text>
+					<text class="right">{{ list.tel }}</text>
+					<text class="right" @click="open()">删除</text>
+				</view>
+			</scroll-view>
+			<uni-popup ref="popup" type="center">
+				<view class="modal-ti">提示</view>
+				<view class="modal-su">确定删除该成员?</view>
+				<view class="modal-btn">
+					<button type="default" size="mini" @click="close()">取消</button>
+					<button type="primary" size="mini" @click="del()">确定</button>
+				</view>
+			</uni-popup>
+		</view>
+	</view>
 </template>
 <script>
-  import noData from '@/components/no-data/no-data.vue'
-  import uniPopup from "@/components/uni-popup/uni-popup.vue"
- import eventBus from '@u/eventBus.js'
-	export default {
-    components: {
-      noData,
-      uniPopup
-    },
-		data() {
-			return {
-        dataList: [{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},
-              {
-                    'name':'张三',
-                    'sex': '女',
-                    'Organization': '全品文教',
-                    'tel': '123'},
-                    {
-                          'name':'张三',
-                          'sex': '女',
-                          'Organization': '全品文教',
-                          'tel': '123'},
-                          {
-                                'name':'张三',
-                                'sex': '女',
-                                'Organization': '全品文教',
-                                'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'},{
-              'name':'张三',
-              'sex': '女',
-              'Organization': '全品文教',
-              'tel': '123'}
-        ],
-			}
-		},
-    watch: {
-      value0(val) {
-        console.log(val)
-      },
-      value1(val) {
-        console.log(val)
-      },
-    },
-    onLoad(options) {
-      console.log(options)
-    },
-    onPullDownRefresh() {
-      // this.showList()
-    },
-    mounted() {
-      // this.showList()
-      // eventBus.$on('change', () => {
-      //   alert(4)
-      // })
-    },
-		methods: {
-      open(){
-         this.$refs.popup.open()
-      },
-      close(){
-          this.$refs.popup.close()
-      },
-      //删除
-      del(){
-        
-      },
-      // async showList (tag = false) {
-      //   const res = await actions.getIndex()
-      //   if (tag) {
-      //     this.dataList = this.dataList.concat(res.data)
-      //   } else {
-      //     this.dataList = res.data
-      //     uni.stopPullDownRefresh()
-      //   }
-      // },
+import noData from '@/components/no-data/no-data.vue';
+import uniPopup from '@/components/uni-popup/uni-popup.vue';
+import eventBus from '@u/eventBus.js';
+export default {
+	components: {
+		noData,
+		uniPopup
+	},
+	data() {
+		return {
+			dataList: [
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				},
+				{
+					name: '张三',
+					sex: '女',
+					Organization: '全品文教',
+					tel: '123'
+				}
+			]
+		};
+	},
+	watch: {
+	},
 
-      goBack () {
-             this.$tools.goBack()
-           },
-           add(){
-             uni.navigateTo({
-               url: './add'
-             })
-           }
+	mounted() {},
+	methods: {
+		open() {
+			this.$refs.popup.open();
+		},
+		close() {
+			this.$refs.popup.close();
+		},
+		//删除
+		del() {},
+
+		goBack() {
+			this.$tools.goBack();
+		},
+		add() {
+			uni.navigateTo({
+				url: './add'
+			});
 		}
 	}
+};
 </script>
 
 <style lang="scss" scoped>
-  .list {
-    padding: 25rpx 20rpx;
-    font-size: 28rpx;
-    .th{
-      background: #7B92F5;
-      padding: 20rpx 0;
-      border-radius: 8rpx;
-      color: #fff;
-    }
-    .tbody{
-      position: relative;
-      padding: 25rpx 10rpx;
-    }
-    .tbody:nth-child(even){
-      background: #f5f5f5;
-    } 
-    .left{
-      width: 20%;
-      text-align: center;
-    }
-    .md{
-      width: calc(35% - 40rpx);
-      text-align: center;
-    }
-    .right{
-      width: 35%;
-      text-align: center;
-    }
-  }
-  .scroll-h {
-    height: 100vh;
-  }
-  .detail {
-    padding: 0rpx 20rpx;
-    font-size: 28rpx;
-    margin-top: 20px;
-    // .btn {
-    //   border-radius: $radius;
-    //   background-color: $main-color;
-    //   line-height: 50rpx;
-    //   height: 50rpx;
-    //   color:#fff;
-    //   text-align: center;
-    //   display: inline-block;
-    // }
-    // .back_{
-    //   display: inline-block;
-    //   color: #0079FF;
-    // }
-    // .text_{
-    //   display: inline-block;
-    //   font-size: 16px;
-    //   width: calc(77.5% - 40rpx);
-    //   text-align: center;
-    // }
-  }
-      .uni-popup__wrapper-box {
-width: 270px;
-height: 179px;
-background: inherit;
-   background-color: rgba(255, 255, 255, 1);
-   border-width: 1px;
-  border: 1px solid rgba(204, 204, 204, 1);
-  border-radius: 8px;
-      box-shadow: none;  
-       .modal-ti{
-         text-align: center;
-         margin-top: 20rpx;
-         font-size: 22px;
-       }
-       .modal-su{
-         text-align: center;
-         margin-top: 20rpx;
-         font-size: 15px;
-       }
-       .modal-btn{
-         text-align: center;
-         border-top: 1px solid rgba(204, 204, 204, 1);
-         margin-top:70rpx;
-         button{
-          margin: 30rpx;
-         }
-       }
-      }
+.list {
+	padding: 25rpx 20rpx;
+	font-size: 28rpx;
+	.th {
+		background: #7b92f5;
+		padding: 20rpx 0;
+		border-radius: 8rpx;
+		color: #fff;
+	}
+	.tbody {
+		position: relative;
+		padding: 25rpx 10rpx;
+	}
+	.tbody:nth-child(even) {
+		background: #f5f5f5;
+	}
+	.left {
+		width: 20%;
+		text-align: center;
+	}
+	.md {
+		width: calc(35% - 40rpx);
+		text-align: center;
+	}
+	.right {
+		width: 35%;
+		text-align: center;
+	}
+}
+.scroll-h {
+	height: 100vh;
+}
+.detail {
+	padding: 0rpx 20rpx;
+	font-size: 28rpx;
+	margin-top: 20px;
+	// .btn {
+	//   border-radius: $radius;
+	//   background-color: $main-color;
+	//   line-height: 50rpx;
+	//   height: 50rpx;
+	//   color:#fff;
+	//   text-align: center;
+	//   display: inline-block;
+	// }
+	// .back_{
+	//   display: inline-block;
+	//   color: #0079FF;
+	// }
+	// .text_{
+	//   display: inline-block;
+	//   font-size: 16px;
+	//   width: calc(77.5% - 40rpx);
+	//   text-align: center;
+	// }
+}
+.uni-popup__wrapper-box {
+	width: 270px;
+	height: 179px;
+	background: inherit;
+	background-color: rgba(255, 255, 255, 1);
+	border-width: 1px;
+	border: 1px solid rgba(204, 204, 204, 1);
+	border-radius: 8px;
+	box-shadow: none;
+	.modal-ti {
+		text-align: center;
+		margin-top: 20rpx;
+		font-size: 22px;
+	}
+	.modal-su {
+		text-align: center;
+		margin-top: 20rpx;
+		font-size: 15px;
+	}
+	.modal-btn {
+		text-align: center;
+		border-top: 1px solid rgba(204, 204, 204, 1);
+		margin-top: 70rpx;
+		button {
+			margin: 30rpx;
+		}
+	}
+}
 </style>
