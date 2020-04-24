a5ce2b3fdfeaeddfef4b8816f3c53656cfa4d86d
srf000001 <1299353948@qq.com>
2020-04-24 15:30:00 +0800

    代码优化

--- pages/attendance/attendance-t/api/leave.js
+++ pages/attendance/attendance-t/api/leave.js
@@ -10,16 +10,16 @@ let leaveApi = {
   addTeacherLeave: '/teacher/leave/add#post', // 新增教职工请假
   updateTeacherLeave: '/teacher/leave/update#post', // 更新教职工请假
   getTeacherLeave: '/teacher/leave/getleaveForMobile#post', // PC 教职工分页查询请假单
-  getLeaveDetail: '/teacher/leave/detailByNumbers/#getWithPara', // 根据请假单号获取请假单全部信息
+  getLeaveDetail: '/teacher/leave/detailByNumbers/#getUrl', // 根据请假单号获取请假单全部信息
   getApprovalLeave: '/leave/approval/getApprovalLeaveByUserCode#post', // 查询归自己审批的请假单
   getCopyLeave: '/leave/copy/getCopyLeaveByUserCode#post', // 查询抄送给自己的请假单
-  recallLeave: '/teacher/leave/withdraw/#getWithPara', // 根据单号撤回教职工请假单
+  recallLeave: '/teacher/leave/withdraw/#getUrl', // 根据单号撤回教职工请假单
   approvalLeave: '/leave/approval/approvalTeacherLeave#postQuery', // 审批请假单
   addStudentLeave: '/student/leave/add#post', // 新增学生请假
   updateStudentLeave: '/student/leave/update#post', // 更新学生请假
   getStudentLeave: '/student/leave/getLeaveForChild#post', // 根据请假人code,申请人code 查询学生的请假单
-  studentLeaveDetail: '/student/leave/detailByNumbers/#getWithPara', // 根据请假单号获取请假单全部信息
-  recallStudentLeave: '/student/leave/withdraw/#getWithPara', // 撤回学生请假单
+  studentLeaveDetail: '/student/leave/detailByNumbers/#getUrl', // 根据请假单号获取请假单全部信息
+  recallStudentLeave: '/student/leave/withdraw/#getUrl', // 撤回学生请假单
 }
 
 for (let val in leaveApi) {
