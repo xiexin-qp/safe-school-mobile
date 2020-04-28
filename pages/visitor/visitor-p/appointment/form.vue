636d992ad81b4253822a3049774dd3d92a90917b
unknown <824876766@qq.com>
2020-04-28 14:08:06 +0800

    代码提交

--- pages/visitor/visitor-p/appointment/form.vue
+++ pages/visitor/visitor-p/appointment/form.vue
@@ -110,10 +110,6 @@ export default {
 			this.formData.address = res.data.address;
 			this.formData.phone = res.data.resMobile;
 			this.formData.duration = res.data.duration + '小时';
-			this.formData.startDate = this.$tools.getDateTime(res.data.accessStartTime).split(' ')[0];
-			this.formData.startTime = this.$tools.getDateTime(res.data.accessStartTime).split(' ')[1];
-			this.formData.endDate = this.$tools.getDateTime(res.data.accessEndTime).split(' ')[0];
-			this.formData.endTime = this.$tools.getDateTime(res.data.accessEndTime).split(' ')[1];
 			this.formData.cause = this.causeNameList.findIndex(item => {
 				return item === res.data.causeName;
 			});
