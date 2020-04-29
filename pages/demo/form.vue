d33439c86b6ad64c9928782e0f80f192e99c0e22
张立 <18702707106@163.com>
2020-04-29 10:54:00 +0800

    配置优化

--- pages/demo/form.vue
+++ pages/demo/form.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
-    <scroll-view @refresherpulling="haha" scroll-y="true"  class="scroll-h">
+		<u-picker v-model="birthdayTag" mode="time"></u-picker>
+    <scroll-view @refresherpulling="haha" scroll-y="true"  class="scroll-h u-bg-fff">
       <view class="qui-fx-ac qui-bd-b item-list">
         <view>姓名：</view>
         <view class="qui-fx-f1"><input class="item-input" v-model="formData.userName" style="text-align: right;" placeholder="请输入姓名" /></view>
@@ -15,34 +16,24 @@
         </view>
       </view>
       <view class="qui-fx-ac qui-bd-b item-list">
-        <view>学号：</view>
-        <view class="qui-fx-f1 qui-fx-je">
-          <input class="item-input" v-model="formData.code" style="text-align: right;" placeholder="请输入学号" />
-        </view>
-      </view>
-      <view class="qui-fx-ac qui-bd-b item-list">
         <view>是否已婚：</view>
         <view class="qui-fx-f1 qui-fx-je">
-          <switch @change="switch1Change" />
+          <u-switch v-model="formData.isMarry"></u-switch>
         </view>
       </view>
       <view class="qui-fx-ac qui-bd-b item-list">
         <view>出生日期：</view>
-        <view class="qui-fx-f1 qui-fx-je">
-          <picker mode="date" :value="formData.birthday" @change="bindTimeChange">
-              <view class="uni-input">{{formData.birthday}}</view>
-          </picker>
+        <view class="qui-fx-f1 qui-fx-je" @click="changeTag('birthdayTag')">
+					{{formData.birthday}}
         </view>
-        <view>></view>
+        <view class="rit-icon"></view>
       </view>
       <view class="qui-fx-ac qui-bd-b item-list">
         <view>角色：</view>
         <view class="qui-fx-f1 qui-fx-je">
-          <picker mode="selector" :value="formData.role" :range="role" @change="chooseRole">
-              {{role[formData.role] || '请选择'}}
-          </picker>
+					{{ formData.role }}
         </view>
-        <view>></view>
+        <view class="rit-icon"></view>
       </view>
       <view class="qui-bd-b item-list">
         <view>身体状况：</view>
@@ -66,14 +57,10 @@
 					<an-upload-img total="3" v-model="imgList" style="margin: 20rpx"></an-upload-img>
 			  </view>
 			</view>
-      <view>
-        {{JSON.stringify(formData)}}
-      </view>
     </scroll-view>
-    <view class="submit-box qui-fx">
-      <view class="btn" @click="confirm">确认框</view>
-      <view class="btn" @click="actionsheet">actionsheet</view>
-    </view>
+		<view class="submit-btn">
+			<u-button type="primary">提交</u-button>
+		</view>
   </view>
 </template>
 
@@ -82,18 +69,14 @@
 	export default {
 		data() {
 			return {
-        role: ['家长', '班主任', '教职工'],
-        formInfo: {},
-        formData: {
-          userName: '张立',
-          sex: '1',
-          code: '1234',
-          isMarry: false,
-          birthday: '请选择',
-          role: 0,
-          healthy: ''
-        },
-				imgList: ['http://canpointtest.com/mobile-img/logo.png']
+				birthdayTag: false,
+				imgList: [],
+				formData: {
+					userName: '',
+					sex: '',
+					birthday: '',
+					isMarry: []
+				}
 			}
 		},
 		components: {
@@ -104,6 +87,10 @@
     async mounted() {
     },
     methods: {
+			changeTag (tag) {
+				alert(2)
+				this[tag] = true
+			},
       confirm () {
         this.$tools.confirm('确定删除吗', () => {
 					console.log(this.imgList)
@@ -114,18 +101,6 @@
         this.$tools.actionsheet(['苹果', '梨子', '香蕉'], (index) => {
           console.log(arr[index])
         })
-      },
-      changeRadio (e) {
-        this.formData.sex = e.target.value
-      },
-      switch1Change (e) {
-        this.formData.isMarry = e.target.value
-      },
-      bindTimeChange (e) {
-        this.formData.birthday = e.target.value
-      },
-      chooseRole (e) {
-        this.formData.role = e.target.value
       },
       checkboxChange (e) {
         this.formData.healthy = e.target.value
@@ -144,7 +119,7 @@
   .item-input {
     width: 100%;
     font-size: 26rpx;
-    color: #999;
+    color: $u-content-color;
   }
   .radio {
     padding-left: 25rpx
@@ -152,18 +127,8 @@
   .checkbox {
     padding: 30rpx 10px 10px 2px;
   }
-  .submit-box {
+  .submit-btn {
     height: 100rpx;
-    .btn {
-      padding:0 20rpx;
-      height: 80rpx;
-      line-height: 80rpx;
-      text-align: center;
-      letter-spacing: 8rpx;
-      margin: 0 20rpx;
-      background-color: $main-color;
-      color:#fff;
-      border-radius: $radius;
-    }
+		padding: 10rpx 20rpx;
   }
 </style>
