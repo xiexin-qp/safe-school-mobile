d7206178c927e6e08ddddbf6404744ad2f2f7436
yuju <1290638426@qq.com>
2020-04-29 17:54:34 +0800

    '宿管'

--- pages/index/module/index.js
+++ pages/index/module/index.js
@@ -3,13 +3,15 @@ import visitor from './visitor.js'
 import door from './door.js'
 import attendance from './attendance.js'
 import news from './news.js'
+import housemaster from './housemaster.js'
 
 const moduleList = [
   ...protal,
   ...visitor,
   ...attendance,
   ...door,
-  ...news
+  ...news,
+  ...housemaster
 ]
 
 export default moduleList
