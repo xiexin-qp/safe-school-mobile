31bfd65abf59b9558503860dd85533dc4a9a01ce
123123 <hefei>
2020-04-29 17:46:51 +0800

    代码提交

--- pages/index/module/index.js
+++ pages/index/module/index.js
@@ -3,12 +3,13 @@ import visitor from './visitor.js'
 import door from './door.js'
 import attendance from './attendance.js'
 import news from './news.js'
+import ncov from './ncov.js'
 
 const moduleList = [
   ...protal,
   ...visitor,
   ...attendance,
-  ...door,
+  ...ncov,
   ...news
 ]
 
