a545887c0d115625420f0ca11e71abe72b89a6c8
unknown <824876766@qq.com>
2020-04-29 15:12:02 +0800

    代码提交

--- pages/ncov/ncov-t/index.vue
+++ pages/ncov/ncov-t/index.vue
@@ -1,25 +1,42 @@
 <template>
-  <view>
-    
-  </view>
+	<view>
+		<view class="calendar"><uni-calendar @change="change"></uni-calendar></view>
+	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-			}
+import { store, actions } from '../store/index.js';
+export default {
+	data() {
+		return {};
+	},
+	components: {},
+	computed: {},
+	async mounted() {
+		const date = new Date()
+		let y = date.getFullYear()
+		let m = date.getMonth() + 1
+		m = m < 10 ? ('0' + m) : m
+		let d = date.getDate()
+		d = d < 10 ? ('0' + d) : d
+		this.day = y + '-' + m + '-' + d
+		this.showList()
+	},
+	methods: {
+		async showList(tag = false) {
+			const req = {
+				userCode: store.userInfo.userCode,
+				day: this.day
+			};
+			const res = await actions.getIndex(req);
+			this.dayInfo = res.data;
 		},
-		components: {
-		},
-    computed: {
-    },
-    async mounted() {
-    },
-    methods: {
-    }
+		change(data) {
+			this.day = data.fulldate;
+			this.showList();
+		}
 	}
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
