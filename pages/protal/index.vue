a3fa4e2a3aa40490f297fc00e25da0770d489990
e <18702707106@163.com>
2020-09-09 09:19:52 +0800

    代码优化

--- pages/protal/index.vue
+++ pages/protal/index.vue
@@ -22,7 +22,6 @@
 
 <script>
 import { setStore, actions } from './store/index.js';
-import wx from 'weixin-js-sdk';
 import uniRequest from 'uni-request';
 import config from '@config';
 export default {
@@ -52,31 +51,6 @@ export default {
 			this.code = '';
 		},
 		// 微信js-api授权
-		getConfig() {
-			const url = '/getTicket';
-			uniRequest
-				.get(url, {
-					params: {
-						path: window.location.href.split('#')[0]
-					}
-				})
-				.then(response => {
-					const res = response.data;
-					wx.config({
-						// debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-						appId: res.appId, // 必填，公众号的唯一标识
-						timestamp: res.timestamp, // 必填，生成签名的时间戳
-						nonceStr: res.nonceStr, // 必填，生成签名的随机串
-						signature: res.signature, // 必填，签名
-						jsApiList: ['openLocation', 'getLocation', 'scanQRCode', 'chooseImage', 'getLocalImgData', 'updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
-					});
-					wx.error(function(res) {});
-					wx.ready(function() {});
-				})
-				.catch(error => {
-					console.log(error); //异常
-				});
-		},
 		// 获取openid
 		async getOpenid() {
 			const url = window.location.href;
