3117b25ea368c8d70144227edd44ed934ca1cbf6
yuju <1290638426@qq.com>
2020-09-16 16:50:56 +0800

    '问题修改'

--- pages/praise-column/praise-column-t/class/index.vue
+++ pages/praise-column/praise-column-t/class/index.vue
@@ -34,70 +34,79 @@ export default {
   },
   data() {
     return {
-      showClass: false,
-      defTitle: "",
-      classMotto: "",
-      length: "0",
       classCode: "",
       gradeCode: "",
       schoolYearId: "",
-      classList: [],
-      value0: "",
       detail: [],
       total: 0,
       pageList: {
         page: 1,
         size: 20,
       },
+       morePage: false,
+      userType: 2, // 0.超管，1.班主任，2.教职工，3.家长
+      value0: "",
+      classList: [],
+      defTitle: "",
+      showClass: false,
     };
   },
-  watch: {
-    classMotto(val) {
-      this.length = val.length;
-    },
-    value0(val, oldval) {
-      console.log(val);
-      if (val !== oldval) {
-        this.defTitle = this.classList.filter((el) => {
-          return el.value === val;
-        })[0].text;
-        this.classCode = val;
-        if (store.isBZR && val === store.isBZR.classCode) {
-          this.userType = "1";
-        } else {
-          this.userType = "0";
-        }
-        this.showList();
-      }
-    },
-  },
-  async created() {
-    this.length = this.classMotto.length;
+ 	watch: {
+		value0(val, oldval) {
+
+			if (val !== oldval) {
+				if (store.isBZR && val === store.isBZR.classCode) {
+					this.userType = 1;
+				} else {
+					this.userType = 2;
+				}
+				let choose = this.classList.filter(el => {
+					return el.value === val;
+				})[0];
+				this.defTitle = choose.text;
+				this.gradeCode = choose.gradeCode;
+				this.classCode = val;
+				uni.setStorageSync('bindInfo', {
+					...this.classList.filter(el => {
+						return el.value === val;
+					})[0],
+					classCode: val
+				});
+				eventBus.$on('getList', () => {
+					this.showList();
+				});
+				this.showList();
+			}
+		}
+	},
+	async created() {
     this.schoolYearId = store.schoolYear.schoolYearId;
-    if (store.userInfo.typeCode === "4") {
-      this.userType = "0";
-      if (store.teachClassList.length === 0) {
-        this.$tools.toast("请绑定班级");
-        return;
-      }
-      this.classList = store.teachClassList;
-      this.classCode = store.teachClassList[0].value;
-      this.gradeCode = store.teachClassList[0].gradeCode;
-      this.showClass = true;
-      this.defTitle = store.teachClassList[0].text;
-      this.value0 = store.teachClassList[0].value;
-      uni.setStorageSync("classInfo", {
-        gradeCode: store.teachClassList[0].gradeCode,
-        classCode: store.teachClassList[0].value,
-        schoolYearId: this.schoolYearId,
-      });
-    }
-    uni.setStorageSync("classInfo", {
-      gradeCode: this.gradeCode,
-      classCode: this.classCode,
-      schoolYearId: this.schoolYearId,
-    });
-  },
+		if (store.userInfo.typeCode === '4') {
+			this.userType = 2;
+			this.classList = JSON.parse(uni.getStorageSync('protal')).teachClassList;
+			if (this.classList.length === 0) {
+				this.$tools.toast('请绑定班级');
+				return;
+			}
+			this.classCode = this.classList[0].value;
+			this.gradeCode = this.classList[0].gradeCode;
+			this.showClass = true;
+			this.defTitle = this.classList[0].text;
+			this.value0 = this.classList[0].value;
+			uni.setStorageSync('bindInfo', {
+				...this.classList[0],
+				classCode: this.classList[0].value
+			});
+		} else if (store.userInfo.typeCode === '16') {
+			this.userType = 3;
+			this.classCode = store.childList[0].classCode;
+			this.gradeCode = store.childList[0].gradeCode;
+			uni.setStorageSync('bindInfo', {
+				...store.childList[0]
+			});
+			this.showList();
+		}
+	},
   mounted() {},
   methods: {
     async showList() {
